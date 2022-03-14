d1db78409026d03a40cc740fa814baf4f0ad3bb1
Neeiser <stortini.study@gmail.com>
2022-03-14 16:15:00 +0100

    Stuck

--- js/main.js
+++ js/main.js
@@ -5,10 +5,39 @@ Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
 - done, un booleano (true/false) che indica se il todo è stato fatto oppure no 
 */
 
+/* 
+MILESTONE 1
+Stampare all'interno di una lista, un item per ogni todo.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, 
+il todo viene rimosso dalla lista.
+
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante "aggiungi": 
+cliccando sul pulsante, il testo digitato viene letto e utilizzato per 
+creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+*/
+
 
 const app = new Vue ({
     el: '.app',
     data: {
-        
+        todo: [
+            {
+                text: 'Bere il latte',
+                done: true,
+            },
+            {
+                text: 'Mangiare una cotoletta',
+                done: false,
+            },
+            {
+                text: 'Lavarsi l\'apparato di golgi',
+                done: true,
+            },
+        ]
     },
+    
 })
